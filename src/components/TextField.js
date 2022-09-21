70950eb54329bee06b8b28047dff2ed8d09effac
ImGKS <101737796+ImGKS@users.noreply.github.com>
2022-09-21 18:24:08 +0530

    Update TextField.js

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -8,7 +8,7 @@ export const TextField = ({ label, ...props }) => {
         <div className="mb-2">
             <label htmlFor={field.name}>{label}</label>
             <input 
-            className={`form-control shadow-none ${meta.touched && meta.error && "is-valid"}`}
+            className={`form-control shadow-none ${meta.touched && meta.error && "is-invalid"}`}
             {...field}{...props}
             autoComplete="off"
             />
